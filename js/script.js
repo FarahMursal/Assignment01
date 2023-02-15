6ba9d606e0100d0bf77f332335fd8adddcb43e13
John Doe <johndoe@example.com>
2023-02-14 21:31:48 -0600

    change2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,48 @@
-let someMonth                      // STEP 1
-function theMonth()                // STEP 2
-let currentMonth                   // STEP 3
-let summerMonth                    // Step 4
-let MmyLibraryFunction             // Step 5 
+// STEP 1
+let someMonth 
+function theMonth()                
+let currentMonth                   
+let summerMonth                    
+let myLibraryFunction              
+
+// Step 2
+11                  // Numeric literal
+"Hi, Friend"        // String Literal
+true                // Boolean Literal
+null                // Null Literal
+
+// Step 3
+let randomExpression = 2 * 5 (3/4) / 8 
+let randomExpressionTwo = 25 / 5 * 8 
+
+// Step 4
+ let strFirstName;
+ let strLastName;
+ let strAddress;
+ let strCity;
+ let strState;
+ let strZipCode;
+ let strReferralSource;
+ let strWeMayContactYou;
+
+ let firstName;
+ let lastName;
+ let address;
+ let city;
+ let state;
+ let zipCode;
+ let referralSource;
+ let weMayContactYou;
+
+ // Step 5
+let firstName, lastName
+firstName = Farah 
+lastName = Mursal
+//or
+let city = San Diego
+let state = California 
+//or
+let address
+let zipCode
+
+// Step 6
