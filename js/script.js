7af144b009c9898953dfd958e53447bfc2b1e79e
John Doe <johndoe@example.com>
2023-02-15 19:26:12 -0600

    some changes

--- js/script.js
+++ js/script.js
@@ -46,5 +46,101 @@ let address
 let zipCode
 
 // Step 6
-let yrsplaying = 10
+let yrsPlaying = 10
 console.log('Farah has been playing basketball for ' + yrsplaying + ' years')
+
+// Step 7
+let isPlaying = true
+let yrsPlaying = 10
+console.log('Farah has been playing ball for ' + String(yrsPlaying) + ' years: ' + isPlaying)
+
+let x = 20
+let y = true
+let total = x + y
+console.log(total)
+
+// Step 8 
+let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former."'
+console.log(someString)
+// This string literal is not valid do to the word "I'm" cuts off the string. 
+// I would add a \ before the ' in "I'm"
+
+// Step 9
+let h = null
+console.log(h)
+
+let w = 12
+let w = 
+console.log(w) 
+
+// Step 10
+let t = 'Hello'
+let u = 10 
+let p = true 
+let g = null
+let v = 
+
+console.log(typeof t)
+console.log(typeof u)
+console.log(typeof p)
+console.log(typeof g)
+console.log(typeof v)
+
+// Step 11
+alert('Hello! ' + 'Farah Mursal' + ', welcome to the Javascript class!')
+
+// Step 12
+let name = Farah Mursal 
+alert('Hello! ' + name + ', welcome to the Javascript class!')
+
+// Step 13
+let course = JavaScript 
+let name = Farah Mursal
+alert('Hello! ' + name + ', welcome to the' + course +  'class!')
+
+//Step 14 
+let course = JavaScript 
+let name = Farah Mursal
+alert('Hello! ' + name + '. \nWelcome to the' + course +  'class!')
+ 
+// Step 15 
+let course = JavaScript 
+let name = prompt('What is your name?')
+alert('Hello! ' + name + '. \nWelcome to the' + course +  'class!')
+
+// Step 16
+let course = prompt('What course are you taking?')
+let name = prompt('What is you name?')
+alert('Hello! ' + name + '. \nWelcome to the' + course +  'class!')
+
+// Step 17
+let x = 10
+let y = 20
+console.log(x + y)
+
+// Step 18
+let x = 20
+x += 20
+console.log(x)
+
+// Step 19
+let x = 20
+x *= 5
+console.log(x)
+
+// Step 20 
+let x = 20 % 3 
+x /= 1 
+console.log(x)
+
+// Step 21
+let age = 22
+let aboveAge = true
+let canDrink = age >= 21 
+console.log('Can individual drink' + canDrink)
+
+// Step 22
+let hieght = 6.1
+let isTall = false
+let onTeam = hieght >= 6.6
+console.log('You made the team: ' + onTeam)
